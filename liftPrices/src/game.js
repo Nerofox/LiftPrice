343248b2b6c0f67ac75020af2cb8d1bfa09efb4e
valentin <renard.valentin@hotmail.fr>
2016-05-03 10:23:27 +0200

    Preparation du projet

---
+++ liftPrices/src/game.js
@@ -0,0 +1,6 @@
+
+//fonction appellée à chaque frame du jeu (environ 30 fois par secondes) ici on detecte les évènements utilisateur
+//(clic de souris par exemple)
+function update() {
+	
+}
---
+++ liftPrices/src/main.js
@@ -0,0 +1,13 @@
+//creation du jeu
+var game = new Phaser.Game(800, 1024, Phaser.AUTO, "AscenseurDuPrix", { preload: preload, create: create });
+
+//ici les fichiers du jeu (image, son etc..) sont préchargés
+function preload() {
+	game.load.image("chat", "ressources/images/chat.png");//importation de l'image on lui donne un nom "chat"
+}
+
+//dans cette section on effectue la création des objets su jeu nommé sprite (bouton, produit)
+function create() {
+	game.add.sprite(0,0, "chat");//creation du sprite en se basant sur le nom chargé ci dessus "chat" avec coordonnées de placement x = 0, y = 0
+}
+
