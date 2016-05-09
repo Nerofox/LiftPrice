7b0f5d187a72a7844cc85f0d2b2444eb2ac75a3f
valentin <renard.valentin@hotmail.fr>
2016-05-09 17:54:33 +0200

    Mise en place template et mouvement porte ascenseur

--- liftPrices/js/lift.js
+++ liftPrices/js/lift.js
@@ -1,3 +1,59 @@
+var outSide;
+var inSide;
+var leftDoor;
+var rightDoor;
+
+var limitXLeft = 314;
+var limitXRight = 743;
+
+var lastParam;
+
 function createLift() {
+	inSide = game.add.sprite(314,443,"inSide");
+	leftDoor = game.add.sprite(314,443,"leftDoor");
+	rightDoor = game.add.sprite(743,443,"rightDoor");
+	outSide = game.add.sprite(0,0, "outSide");
+
+	game.physics.enable(leftDoor, Phaser.Physics.ARCADE);
+	game.physics.enable(rightDoor, Phaser.Physics.ARCADE);
+}
+
+/**
+ * Animation ouvre et ferme la porte
+ * @param Boolean openOrClose
+ */
+function doorLift(openOrClose) {
+	lastParam = openOrClose;
+
+	if (openOrClose == true) {
+		//porte droite
+		rightDoor.body.velocity.x = 800;
+		limitXRight = 1162;
+		//porte gauche
+		leftDoor.body.velocity.x = -800;
+		limitXLeft = -100;
+	} else {
+		//porte droite
+		rightDoor.body.velocity.x = -800;
+		limitXRight = 743
+		//porte gauche
+		leftDoor.body.velocity.x = 800;
+		limitXLeft = 314;
+	}
+}
+
+function updateLift() {
+	//gestion d'arrêt des mouvements des portes
+	if (lastParam == true) {
+		if (rightDoor.body.x >= limitXRight)
+			rightDoor.body.velocity.x = 0;
+		if (leftDoor.body.x <= limitXLeft)
+			leftDoor.body.velocity.x = 0;
+	} else {
+		if (rightDoor.body.x <= limitXRight)
+			rightDoor.body.velocity.x = 0;
+		if (leftDoor.body.x >= limitXLeft)
+			leftDoor.body.velocity.x = 0;
+	}
 	
 }
--- liftPrices/js/main.js
+++ liftPrices/js/main.js
@@ -4,17 +4,37 @@ var game;
 startGame();
 
 function startGame() {
-	game = new Phaser.Game(1536, 2048, Phaser.AUTO, "AscenseurDuPrix", { preload: preload, create: create });;
+	game = new Phaser.Game(1536, 2048, Phaser.AUTO, "AscenseurDuPrix", { preload: preload, create: create, update: update });;
 }
 
 //ici les fichiers du jeu (image, son etc..) sont préchargés
 function preload() {
-	//game.load.image("chat", "ressources/images/chat.png");//importation de l'image on lui donne un nom "chat"
+	game.load.image("outSide", "param/img/lift/outSide.png");
+	game.load.image("inSide", "param/img/lift/inSide.png");
+	game.load.image("leftDoor", "param/img/lift/leftDoor.png");
+	game.load.image("rightDoor", "param/img/lift/rightDoor.png");
 }
 
 //dans cette section on effectue la création des objets su jeu nommé sprite (bouton, produit)
 function create() {
+	game.input.onTap.add(test);
+
 	createLift();
 	//game.add.sprite(0,0, "chat");//creation du sprite en se basant sur le nom chargé ci dessus "chat" avec coordonnées de placement x = 0, y = 0
 }
 
+function update() {
+	updateLift();
+	updateGame();
+}
+
+
+var testParam = true;
+function test() {
+	doorLift(testParam);
+	if (testParam) {
+		testParam = false;
+	} else {
+		testParam = true;
+	}
+}
