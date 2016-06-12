77cda4a1befc5e096ff35c2c99ec2d4931fad415
Valentin <renard.valentin@hotmail.fr>
2016-06-12 19:08:42 +0200

    integration idol

--- liftPrices/js/functions.js
+++ liftPrices/js/functions.js
@@ -6,7 +6,20 @@
  * Use idol-tickets.js to manage tickets game
  */
 
+/**
+ * JEU DE LASCENSEUR DU PRIX
+ * Réalisé par Valentin Renard & Celine Meunier
+ * Le 11/06/2016
+ * Version : 1.0
+ */
 
+var isMobile = new RegExp('/iphone|ipad|ipod|android|blackberry/i').test(navigator.userAgent.toLowerCase());
+if (!isMobile) {
+    $(window).load(function () {
+        console.log('Demarrage depuis le navigateur');
+        startModule(context);
+    });
+}
 
 /**
 * Module initialization
